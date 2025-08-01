4fd2cb20ffc0f204f944deaee924b0e440c0f32b
MitulNakrani003 <mitulnakrani3010@gmail.com>
2025-08-01 19:15:58 -0400

    first commit

---
+++ src/main/java/com/mandm/harmony_hr/HarmonyHrApplication.java
@@ -0,0 +1,13 @@
+package com.mandm.harmony_hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyHrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mandm/harmony_hr/HarmonyHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandm.harmony_hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
