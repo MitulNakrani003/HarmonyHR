3557a5fcd8db688437994a536db11bea4f58dbea
MitulNakrani003 <mitulnakrani3010@gmail.com>
2025-08-13 23:47:32 -0400

    Working - CQRS permissions added and Tested sign in and signup with JWT response

--- src/main/java/com/mandm/harmony_hr/HomeController.java
+++ src/main/java/com/mandm/harmony_hr/HomeController.java
@@ -5,12 +5,15 @@ import com.mandm.harmony_hr.services.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/home")
 public class HomeController {
 
     private final EmployeeService employeeService;
@@ -24,6 +27,24 @@ public class HomeController {
     public ResponseEntity<List<Employee>> home() {
         List<Employee> employees = employeeService.getAllEmployees();
         return ResponseEntity.ok(employees);
+    }
+
+
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('EMPLOYEE') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
     }
 
 }
--- src/main/java/com/mandm/harmony_hr/dto/MessageResponse.java
+++ src/main/java/com/mandm/harmony_hr/dto/MessageResponse.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class MessageResponse {
     private String message;
-
     public MessageResponse(String message) {
         this.message = message;
     }
